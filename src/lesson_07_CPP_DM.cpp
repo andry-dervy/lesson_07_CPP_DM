e02884ef0d16a51d6e0fb931a7060e51eb9b145a
Andry Antonenko <dervy@mail.ru>
2022-01-18 20:02:03 +0300

    Solved task.

--- src/lesson_07_CPP_DM.cpp
+++ src/lesson_07_CPP_DM.cpp
@@ -40,15 +40,71 @@ class IMethods {
 //*/
 void task_2_3()
 {
+  std::cout << "Task 2, 3" << std::endl;
 
+  std::cout << "\nTest 1\n" << std::endl;
 
+  const int NUMBER_ESTIMATIONS = 5;
+  const int MIN_ESTIMATION = 0;
+  const int MAX_ESTIMATION = 100;
+
+  sgpb::StudentsGroup group;
+  std::shared_ptr<sg::Student> st1 = std::make_shared<sg::Student>();
+  {
+    sg::FullName* pname = new sg::FullName;
+    pname->set_m_surname("Family");
+    pname->set_m_name("Name");
+    pname->set_m_patronymic("Patronymic");
+    st1->set_allocated_m_fullname(pname);
+
+    for(int i = 0; i < NUMBER_ESTIMATIONS; ++i)
+      st1->add_m_estimation(getRandomNum(MIN_ESTIMATION,MAX_ESTIMATION));
+  }
+  group.addStudent(st1);
+
+  std::cout << group.GetAllInfo() << std::endl;
+
+  std::cout << "\nTest 2\n" << std::endl;
+
+  std::cout << group.GetAllInfo(st1->m_fullname()) << std::endl;
+
+  std::cout << "\nTest 3\n" << std::endl;
+  {
+    std::shared_ptr<sg::Student> st = std::make_shared<sg::Student>();
+    {
+      sg::FullName* pname = new sg::FullName;
+      pname->set_m_surname("Petrov");
+      pname->set_m_name("Ivan");
+      st->set_allocated_m_fullname(pname);
+
+      for(size_t i = 0; i < NUMBER_ESTIMATIONS; ++i)
+        st->add_m_estimation(getRandomNum(MIN_ESTIMATION,MAX_ESTIMATION));
+    }
+    group.addStudent(st);
+  }
+
+  std::cout << group.GetAllInfo();
+
+  std::cout << "\nTest 4\n" << std::endl;
+
+  std::cout << "Save group." << std::endl;
+  group.Save();
+
+  sgpb::StudentsGroup group_two;
+  std::cout << "Open group_two." << std::endl;
+  group_two.Open();
+  std::cout << group_two.GetAllInfo();
+
+  std::cout << std::endl;
 }
-
 //----------------------------------------------------------------------------
 int main()
 {
   //--------------------------------------------------------------------------
-
+  // Task 2, 3
+  //*
+  task_2_3();
+  //*/
   //--------------------------------------------------------------------------
   return 0;
 }
